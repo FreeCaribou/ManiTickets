bd7086ae48b98ae09cdbedc9f924eaa653ee90ef
Caribou <kingdomflo16@gmail.com>
2023-10-05 16:12:17 +0200

    try link to the mail

---
+++ manifiesta-tickets/app/api/email/route.ts
@@ -0,0 +1,30 @@
+// TODO protect this route
+export async function GET(request: Request) {
+    console.log('HELLO we try send mail')
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+    const msg = {
+        to: '',
+        from: '',
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+    // sgMail.send(msg);
+
+    (async () => {
+        try {
+            await sgMail.send(msg);
+        } catch (error) {
+            console.log('PERKELE', process.env.NEXT_PUBLIC_SENDGRID_API_KEY)
+            console.log(error);
+            // console.log(error.response);
+        }
+    })();
+
+    return Response.json({
+        hello: 'world'
+    });
+}
--- manifiesta-tickets/components/Admin/Admin.tsx
+++ manifiesta-tickets/components/Admin/Admin.tsx
@@ -1,13 +1,36 @@
 'use client'
 
-import { Title, Text } from '@mantine/core';
+import { Title, Text, Button } from '@mantine/core';
+import axios from 'axios';
 
 export function Admin() {
+
+    function onSendTestMailClick() {
+        console.log('you have click bro, nice')
+
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+        const msg = {
+            to: '',
+            from: '',
+            subject: 'Sending with Twilio SendGrid is Fun',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        };
+    
+        sgMail.send(msg);
+
+        axios.get(`${window.location.origin}/api/email`).then(d => {
+            console.log('data of get send mail', d)
+            // TODO the else, show what happen
+        });
+    }
 
     return (
         <>
             <Title ta="center" c="colorPrimary">Admin Page</Title>
             <Text>Hello mister admin</Text>
+            <Button onClick={onSendTestMailClick}>Test Send Mail</Button>
         </>
     )
 }
