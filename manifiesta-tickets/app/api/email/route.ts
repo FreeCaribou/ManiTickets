0d241def3e85017711fa61aaab74cdfcada2e56c
FreeCaribou <florian@gerardweb.eu>
2023-10-07 09:20:34 +0200

    Mailing

--- manifiesta-tickets/app/api/email/route.ts
+++ manifiesta-tickets/app/api/email/route.ts
@@ -5,8 +5,8 @@ export async function GET(request: Request) {
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
     const msg = {
-        to: '',
-        from: '',
+        to: process.env.TEST_MAIL,
+        from: process.env.TEST_MAIL,
         subject: 'Sending with Twilio SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
